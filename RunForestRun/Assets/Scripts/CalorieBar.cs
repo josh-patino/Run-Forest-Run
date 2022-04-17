421c86562b4ebfde4ac8ad39c3c4b67cfb56b43c
King-Julian11 <blazerman741@gmail.com>
2022-04-17 13:50:13 -0500

    Calorie values

---
+++ RunForestRun/Assets/Scripts/CalorieBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalorieBar : MonoBehaviour
+{
+    public int calories = 2500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
