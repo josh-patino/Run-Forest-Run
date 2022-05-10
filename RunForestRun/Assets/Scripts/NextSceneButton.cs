efaa01b6a7eea15125693b67ab147a1b70b7e174
Quinnimy <qlamkin@mail.bradley.edu>
2022-05-10 18:18:09 -0500

    Start of Title screen

---
+++ RunForestRun/Assets/Scripts/NextSceneButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class NextSceneButton : MonoBehaviour
+{
+    private Button button;
+
+    //private StoryManager storyManager;
+
+    public int difficulty;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(SetDifficulty);
+
+        //storyManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<StoryManager>();
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+        // storyManager.NextScene();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
---
+++ RunForestRun/Assets/Scripts/StoryManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class StoryManager : MonoBehaviour
+{
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
