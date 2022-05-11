cd31a5e72b8d14cf2f06891d7f507966db789c1d
Zechariah Burrus <zechburrus@gmail.com>
2022-05-10 21:13:21 -0500

    Update to time and UI

--- RunForestRun/Assets/Scripts/Timer.cs
+++ RunForestRun/Assets/Scripts/Timer.cs
@@ -1,53 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class Timer : MonoBehaviour
-{
 
+public class Timer : MonoBehaviour {
     public LevelManager levelManager;
     private PlayerController playerControllerScript;
 
 
     public float timeLeft;
     public Text timeText;
+
     public float totalTime;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        if (timeText == null)
-        {
+    void Start() {
+        if (timeText == null) {
             timeText = FindObjectOfType<Text>();
         }
+
         timeText.gameObject.SetActive(true);
 
 
-        if (levelManager == null)
-        {
+        if (levelManager == null) {
             levelManager = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<LevelManager>();
         }
-        //totalTime = (SceneManager.GetActiveScene().buildIndex + 1) * 15f;
-        //works kinda
-        // timeLeft = levelManager.timeLeft;
         timeLeft = (SceneManager.GetActiveScene().buildIndex + 1) * 15f;
-
-        //timeText.text = "Time Left: " + timeLeft;
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-        //timeLeft += Time.deltaTime;
-        timeLeft -=  Time.deltaTime;
-
-
-        timeText.text = "Time Left: " + timeLeft;
-
-       /* if (!playerControllerScript.gameOver)
-        {
-            timeText.text = "Time Left: " + timeLeft;
-        }*/
+    void Update() {
+        timeLeft -= Time.deltaTime;
+        timeText.text = "Time Left: " + Math.Round(timeLeft);
     }
 }
