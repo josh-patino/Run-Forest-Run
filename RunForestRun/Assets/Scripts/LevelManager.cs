e26d52a3a5c256399bf2f526be3fba745d57f2f3
Zechariah Burrus <zechburrus@gmail.com>
2022-05-10 20:14:59 -0500

    Fixed animations and player collider

--- RunForestRun/Assets/Scripts/LevelManager.cs
+++ RunForestRun/Assets/Scripts/LevelManager.cs
@@ -3,6 +3,7 @@
 * Project 4
 * changes the level when the player gets to the end of each level
 */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,25 +12,19 @@ using UnityEngine.SceneManagement;
 //attach to scenemanager
 //keeps track of time to progress levels as time would be closest thing to distance variable we have
 
-public class LevelManager : MonoBehaviour
-{
-
+public class LevelManager : MonoBehaviour {
     public float timeLeft;
     public float time;
     int nextSceneIndex;
 
     private PlayerController playerControllerScript;
-
-
-
-
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        if (playerControllerScript == null)
-        {
+    void Start() {
+        if (playerControllerScript == null) {
             playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         }
+
         //grab current scene
         nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
 
@@ -37,16 +32,12 @@ public class LevelManager : MonoBehaviour
         time = Time.time;
         //changes how long each stage lasts adding time each new stage
         timeLeft = (SceneManager.GetActiveScene().buildIndex + 1) * 20f;
-        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if ((Time.time >= time + timeLeft) && (playerControllerScript.gameOver != true))
-        {
-            if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
-            {
+    void Update() {
+        if ((Time.time >= time + timeLeft) && (playerControllerScript.gameOver != true)) {
+            if (SceneManager.sceneCountInBuildSettings > nextSceneIndex) {
                 SceneManager.LoadScene(nextSceneIndex);
             }
         }
