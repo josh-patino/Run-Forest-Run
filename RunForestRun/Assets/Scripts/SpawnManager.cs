bb1f7c77f78073495f7b87003471b661f77d01f7
Zechariah Burrus <zechburrus@gmail.com>
2022-05-10 19:26:00 -0500

    Reworked the spawning system to support lanes. Prefabs are no longer spawning inside of eachtoher and the vehicle and food routines are seperated to support more dynamic spawn times

--- RunForestRun/Assets/Scripts/SpawnManager.cs
+++ RunForestRun/Assets/Scripts/SpawnManager.cs
@@ -3,65 +3,107 @@
 * Project 4
 * manages the car and food spawns
 */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.SocialPlatforms;
+using Random = UnityEngine.Random;
 
-public class SpawnManager : MonoBehaviour
-{
-    public GameObject[] prefabsToSpawn;
-    private float leftBound = -6.5f; 
-    private float rightBound = 6.5f; 
-    private float spawnPosZ = 210.0f; 
-    private PlayerController playerControllerScript; 
-    public float minRangeInSec = 0.3f; 
-    public float maxRangeInSec = 0.8f; 
+public class SpawnManager : MonoBehaviour {
+    [FormerlySerializedAs("prefabsToSpawn")] public GameObject[] vehiclePrefabs;
+    private float spawnPosX = 210.0f;
+    private PlayerController playerControllerScript;
+    public float minRangeInSec = 0.3f;
+    public float maxRangeInSec = 0.8f;
+    public float foodTimeModifier = 2f;
+
+    public int laneCount = 6;
+    public float laneWidth = 0f;
+    public float playerBoundary = 6.5f;
+    List<float> laneMiddlePoint = new List<float>();
+    private int randomLane = 0;
+    private int lastSpawnedLane = 0;
     
-    public GameObject[] foodPrefabs; 
+    public GameObject[] foodPrefabs;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>(); 
-        StartCoroutine(SpawnRandomPrefabWtihCoroutine()); 
+    void Start() {
+        DefineLanes(playerBoundary);
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        StartCoroutine(SpawnRandomVehiclePreFabWithCoroutine());
+        StartCoroutine(SpawnRandomFoodPrefabWithCoroutine());
     }
-    IEnumerator SpawnRandomPrefabWtihCoroutine()
-    {
-        //add a 3 second delay before first spawning object
+
+    IEnumerator SpawnRandomVehiclePreFabWithCoroutine() {
         yield return new WaitForSeconds(3f);
-        while (!playerControllerScript.gameOver)
-        {
-            SpawnRandomPreFab();
+        while (!playerControllerScript.gameOver) {
+            //pick a random car
+            int prefabIndex = Random.Range(0, vehiclePrefabs.Length);
+        
+            //Pick a random lane making sure we don't pick the last lane that spawned something.
+            while (randomLane == lastSpawnedLane) {
+                randomLane = Random.Range(0, laneMiddlePoint.Count);
+                Debug.Log("Random Lane Chosen: " + randomLane);
+            }
+            //Update the last spawned lane.
+            lastSpawnedLane = randomLane;
+        
+            //Generate the spawn position based on the lane
+            Vector3 spawnPos = new Vector3(spawnPosX, 0, laneMiddlePoint[randomLane]);
 
-            float randomDelay = Random.Range(minRangeInSec, maxRangeInSec); 
-
+            //Create the Vehicle
+            Instantiate(vehiclePrefabs[prefabIndex], spawnPos, vehiclePrefabs[prefabIndex].transform.rotation);
+            
+            //Random delay
+            float randomDelay = Random.Range(minRangeInSec, maxRangeInSec);
             yield return new WaitForSeconds(randomDelay);
         }
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    IEnumerator SpawnRandomFoodPrefabWithCoroutine() {
+        yield return new WaitForSeconds(3f);
+        while (!playerControllerScript.gameOver) {
+            //Pick a random food
+            int prefabFoodIndex = Random.Range(0, foodPrefabs.Length);
         
-    }
-
-    void SpawnRandomPreFab()
-    {
-        //pick a random car
-            int prefabIndex = Random.Range(0,prefabsToSpawn.Length);
-            int prefabFoodIndex = Random.Range(0,foodPrefabs.Length);
-
-            // generate random spawn position
-            Vector3 spawnPos = new Vector3(spawnPosZ, 0, Random.Range(leftBound,rightBound));
-            Vector3 spawnPosFood = new Vector3(spawnPosZ, 0, Random.Range(leftBound,rightBound));
-
-            // random animal to spawn
-            Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
-            Instantiate(foodPrefabs[prefabFoodIndex], spawnPosFood, foodPrefabs[prefabFoodIndex].transform.rotation);
+            //Pick a random lane making sure we don't pick the last lane that spawned something.
+            while (randomLane == lastSpawnedLane) {
+                randomLane = Random.Range(0, laneMiddlePoint.Count);
+                Debug.Log("Random Lane Chosen: " + randomLane);
+            }
+            //Update the last spawned lane.
+            lastSpawnedLane = randomLane;
+        
+            //Generate the spawn position based on the lane
+            Vector3 spawnPos = new Vector3(spawnPosX, 0, laneMiddlePoint[randomLane]);
+        
+            //Create the food
+            Instantiate(foodPrefabs[prefabFoodIndex], spawnPos, foodPrefabs[prefabFoodIndex].transform.rotation);
             
+            //Random delay
+            float randomDelay = Random.Range(minRangeInSec * foodTimeModifier, maxRangeInSec * foodTimeModifier);
+            yield return new WaitForSeconds(randomDelay);
+        }
     }
-
     
+    //Boundary represents how far in a single direction the player controller is able to move.
+    void DefineLanes(float boundary) {
+        //Define the lane width as the total distance of the player boundary
+        //Divided by the total lane count
+        laneWidth = (boundary * 2) / laneCount;
+        //Initialize the leftmostbound as the bound provided
+        float leftmostBound = boundary;
+        for (int i = 0; i < laneCount; i++) {
+            float rightmostBound = leftmostBound - laneWidth;
+            //Add the middle point of the leftmostbound and the rightmost bound of each lane to the list
+            //for the total lane count
+            laneMiddlePoint.Add((leftmostBound + rightmostBound)/2);
+            //Update the leftmostbound to the right bound of the added lane
+            leftmostBound = (leftmostBound - laneWidth);
+        }
+    }
 }
